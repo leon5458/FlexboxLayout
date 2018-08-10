05f4178e62d29c1548666a21396014f548839289
胡小牧 <919633712@qq.com>
2018-08-10 16:04:02 +0800

    push

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # FlexboxLayout
 
+![](https://github.com/leon5458/FlexboxLayout/blob/master/FlexboxLayout/1.png)  
+### FlexboxLayout 实现标签的效果
--- app/src/main/java/hly/com/flexboxlayout/MainActivity.java
+++ app/src/main/java/hly/com/flexboxlayout/MainActivity.java
@@ -23,13 +23,13 @@ public class MainActivity extends AppCompatActivity {
             TextView textView = new TextView(this);
             textView.setBackgroundResource(R.drawable.text_circle_gray);
             textView.setText(arr[i]);
-            textView.setTextColor(0xffa8c4d7);
-            textView.setGravity(Gravity.CENTER);
-            textView.setPadding(10, 5, 10, 5);
-            textView.setTextSize(13);
+            textView.setTextColor(0xffa8c4d7);//根据需求自己设置
+            textView.setGravity(Gravity.CENTER);//根据需求自己设置
+            textView.setPadding(10, 5, 10, 5);//根据需求自己设置
+            textView.setTextSize(13);//根据需求自己设置
             FlexboxLayout.LayoutParams params = new FlexboxLayout.LayoutParams(FlexboxLayout.LayoutParams.WRAP_CONTENT, FlexboxLayout.LayoutParams.WRAP_CONTENT);
-//            params.setFlexBasisPercent(0.2f);//相对父类所占的比例
-            params.setMargins(20, 10, 0, 0);
+//            params.setFlexBasisPercent(0.2f);//相对父类所占的比例 ,根据需求自己设置
+            params.setMargins(20, 10, 0, 0);//根据需求自己设置
             textView.setLayoutParams(params);
             flex.addView(textView);
 
